c54f8732183fff86b5f8613300c59d16428cb991
Jeff Baumgardt <jbaumgardt@caseshare.com>
2018-01-05 14:39:09 -0700

    Add flowtyped definations where available

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Provider } from 'react-redux'
 import { Route } from 'react-router'
 import { Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
-import { store } from './store'
 import { history } from './store/navigation'
+import { store } from './store'
 
 const App = () => {
 	return (
