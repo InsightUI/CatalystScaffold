be65b117ffc77a9a0116e505c4dea30de9b10059
Jeff Baumgardt <jbaumgardt@catalystsecure.com>
2018-02-27 16:44:29 -0700

    Use pritter

--- src/Title.js
+++ src/Title.js
@@ -2,19 +2,25 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { updateTitleAction } from './utils/documentTitle'
 
-const Title = (props) => {
-  const { title, updateTitleAction } = props
+const Title = props => {
+	const { title, updateTitleAction } = props
 
-  return (
-    <div>
-      <h1>{title}</h1>
-      <input type='text' defaultValue={title} onChange={(e) => { updateTitleAction(e.target.value) }} />
-    </div>
-  )
+	return (
+		<div>
+			<h1>{title}</h1>
+			<input
+				type="text"
+				defaultValue={title}
+				onChange={e => {
+					updateTitleAction(e.target.value)
+				}}
+			/>
+		</div>
+	)
 }
 
-const mapStateToProps = (state) => ({
-  title: state.title
+const mapStateToProps = state => ({
+	title: state.title
 })
 
 export default connect(mapStateToProps, { updateTitleAction })(Title)
